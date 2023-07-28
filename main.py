cb81d9f703b4428a5aedc991a94cf3014fa90353
Awais Amjad <amjadawais08@gmail.com>
2023-07-28 16:23:03 +0100

    circuit image and instructions

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import serial
 
 
-ser = serial.Serial('COM7', 9600)  #
+ser = serial.Serial('COM7', 9600)
 ser.timeout = 0.1
 
 
@@ -11,48 +11,57 @@ def detect_blue_object(frame):
     # Convert the frame to HSV color space
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-
-    # Define the lower and upper range for blue color (you may need to adjust these values)
     lower_blue = np.array([90, 50, 50])
     upper_blue = np.array([130, 255, 255])
-
 
     # Create a binary mask to isolate blue pixels
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
 
-
     # Find contours in the binary image
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-
-    # Check if any contours are found
     if len(contours) > 0:
-        # Draw the contours on the original frame
         for contour in contours:
             cv2.drawContours(frame, [contour], -1, (0, 255, 0), 2)
 
-
         # Get the area of the largest contour (assuming the blue object is the largest)
         contour_area = max(cv2.contourArea(contour) for contour in contours)
-
-
+        
         # Calculate the area of the ROI
         roi_area = frame.shape[0] * frame.shape[1]
-
-
+        
         # Check if the blue object occupies at least 20% of the small frame
         if contour_area >= 0.2 * roi_area:
             print("Blue Object Detected")
             ser.write(b'1')  # Send '1' to Arduino to turn on the green LED
         else:
-            ser.write(b'0')
-
+            ser.write(b'0') # send '0' to turn red LED on 
 
     return frame
 
+def instructionsFrame():
+    width, height = 640, 480
+    canvas = 255 * np.ones((height, width, 3), dtype=np.uint8)
+
+    # Define the instructions text and font settings
+    instructions_text = "Place a Blue object in the square"
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    font_scale = 1
+    font_thickness = 2
+    font_color = (0, 0, 0)  # Black color (in BGR format)
+
+    # Calculate the text size to center the text on the canvas
+    (text_width, text_height), _ = cv2.getTextSize(instructions_text, font, font_scale, font_thickness)
+    text_x = int((width - text_width) / 2)
+    text_y = int((height + text_height) / 2)
+
+    # Draw the instructions text on the canvas
+    cv2.putText(canvas, instructions_text, (text_x, text_y), font, font_scale, font_color, font_thickness)
+    
+    return cv2.imshow('Instructions', canvas)
+
 
 cap = cv2.VideoCapture(0)
-
 
 while True:
     ret, frame = cap.read()
@@ -83,8 +92,9 @@ while True:
 
 
         cv2.imshow('Blue Detection', frame)
+        instructionsFrame()
 
-
+    # Press q or x button to close window
     if cv2.waitKey(1) & 0xFF == ord('q') or cv2.getWindowProperty('Blue Detection', cv2.WND_PROP_VISIBLE) < 1:
         break
 
